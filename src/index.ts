b3978cd9cad4320eb1c29e3893f80f62872ed6c7
Rajesh Babu <sammydave.e@gmail.com>
2023-05-27 13:39:10 +0200

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import './index.css';
+
+export { Button, buttonVariants, type ButtonProps } from './components/Button';
+export { Avatar, AvatarFallback, AvatarImage } from './components/Avatar';
+export {Alert, AlertDescription, AlertTitle } from './components/Alert';
---
+++ vite.config.ts
@@ -0,0 +1,29 @@
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
+import { peerDependencies } from './package.json'
+import react from '@vitejs/plugin-react'
+import dts from 'vite-plugin-dts';
+
+export default defineConfig({
+  plugins: [
+    react({
+      'jsxRuntime': 'classic'
+    }),
+    dts({
+      include: ['src/**/*'],
+    })
+  ],
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src', 'index.ts'),
+      formats: ['es', 'cjs'],
+      fileName: (ext) => `index.${ext}.js`,
+    },
+    rollupOptions: {
+      external: [...Object.keys(peerDependencies)], output: { preserveModules: true, exports: 'named' }
+    },
+    
+    target: 'esnext',
+    sourcemap: true
+  }
+})
