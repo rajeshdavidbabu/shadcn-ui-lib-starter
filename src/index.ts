e0186393c038cd15c2b6000f12f4a49300a5820c
Rajesh Babu <sammydave.e@gmail.com>
2023-05-28 17:35:23 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,3 @@
-import './index.css';
-
 export { Button, buttonVariants, type ButtonProps } from './components/Button';
 export { Avatar, AvatarFallback, AvatarImage } from './components/Avatar';
 export {Alert, AlertDescription, AlertTitle } from './components/Alert';
